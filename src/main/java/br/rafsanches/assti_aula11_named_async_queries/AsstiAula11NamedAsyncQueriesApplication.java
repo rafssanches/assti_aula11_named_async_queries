46d58f36a0ffd50276fb8e36735c2c61be37dd82
rafsanches <rafael.ssanches95@gmail.com>
2019-10-30 19:58:53 -0300

    Atividade 11 Queries SpringData

---
+++ src/main/java/br/rafsanches/assti_aula11_named_async_queries/AsstiAula11NamedAsyncQueriesApplication.java
@@ -0,0 +1,13 @@
+package br.rafsanches.assti_aula11_named_async_queries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsstiAula11NamedAsyncQueriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsstiAula11NamedAsyncQueriesApplication.class, args);
+	}
+
+}
